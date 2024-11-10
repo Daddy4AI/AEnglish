13c890071bfc7227982417d8dfa39045a501ba87
Daddy4AI <alanqwyuan@gmail.com>
2024-11-11 00:09:53 +0800

    folders

---
+++ chinese/firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCHA4LX8ZLlUQ6Xk4v9uJ_913Vox1x5Pjg",
+  authDomain: "achinese-14937.firebaseapp.com",
+  databaseURL: "https://achinese-14937-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "achinese-14937",
+  storageBucket: "achinese-14937.appspot.com",
+  messagingSenderId: "138430106435",
+  appId: "1:138430106435:web:2d42a8ed386d4a3e667df6",
+  measurementId: "G-20WCZQ3YE2"
+
+};
